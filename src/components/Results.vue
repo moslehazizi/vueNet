36dea37b8561936f7fc5c4edcf9183c2bdad33ad
moslehazizi <moslehazizi@gmail.com>
2024-03-23 23:40:02 +0330

    Add Block and Results components

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>Reaction time - {{ result }} ms </p>
+    <p class="rank"> {{ rank }} </p>
+</template>
+
+<script>
+export default {
+    props: ['result'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.result < 250) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.result < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail Pace'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4rem;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
